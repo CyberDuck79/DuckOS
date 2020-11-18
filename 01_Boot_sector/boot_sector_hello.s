b9268b01e9a6a34f32889da8895d3c09f7897b7e
Flavien Henrion <flavien.henrion@protonmail.com>
2020-11-18 16:21:51 +0100

    print string 1

--- 01_Boot_sector/boot_sector_hello.s
+++ 01_Boot_sector/boot_sector_hello.s
@@ -1,15 +1,35 @@
+[org 0x7c00] ; offset the starting address when direct addressing
+
 mov ah, 0x0e ; tty mode
-mov al, 'H'
+mov bx, hello_world
+mov al, [bx]
+print1:
+	int 0x10
+	add bx, 1
+	mov al, [bx]
+	test al, al
+	jnz print1
+mov al, [return]
 int 0x10
-mov al, 'e'
-int 0x10
-mov al, 'l'
-int 0x10
-int 0x10 ; 'l' is still on al
-mov al, 'o'
+mov bx, boot_msg
+mov al, [bx]
+print2:
+	int 0x10
+	add bx, 1
+	mov al, [bx]
+	test al, al
+	jnz print2
+mov al, [return]
 int 0x10
 
 jmp $ ; jump to current address = infinite loop
+
+hello_world:
+	db `hello world\n\0`
+boot_msg:
+	db `Booting DuckOS\n\0`
+return:
+	db `\r`
 
 ; padding and magic number
 times 510 - ($-$$) db 0
